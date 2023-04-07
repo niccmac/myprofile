59002079cd033cb6e0634f72c0fbe9cfe241be9a
Nicole MacLean <nicole.mac0404@gmail.com>
2023-04-07 13:50:04 -0700

    Make work mobile friendly

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -26,18 +26,39 @@ import {
   AccordionButton,
   AccordionPanel,
   AccordionItem,
-  AccordionIcon,
+  AccordionIcon
 } from "@chakra-ui/react";
-import { useState } from "react";
-import { VscLink } from "react-icons/vsc";
 
 // Send button colours
 // TODO get tool tip working. Appears after alert
 // TODO fix divider full width
 export default function Work() {
-  const { isOpen, onToggle } = useDisclosure();
-
   const jobs = [
+    {
+      role: "Junior Dev",
+      company: "theNumber, Victoria BC",
+      timeline: "Sep 2022 - NOW",
+      link: "https://thenumber.ca/",
+      icon: "software-engineer.png",
+      description: (
+        <>
+          <u>ENGINEERING</u>
+          <Text size="5xl" align="flex-start">
+            • Assisting the development manager with aspects of software design
+            and coding.
+          </Text>
+          <Text alignSelf="flex-start">
+            • Attending and contributing to company development meetings.
+          </Text>
+          <Text alignSelf="flex-start">
+            • Learning the codebase and improving your coding skills.
+          </Text>
+          <Text alignSelf="flex-start">• Writing and maintaining code.</Text>
+          <Text alignSelf="flex-start"> • Working on minor bug fixes.</Text>
+        </>
+      )
+    },
+
     {
       role: "Lead Distiller",
       company: "Victoria Distillers",
@@ -47,24 +68,26 @@ export default function Work() {
       description: (
         <>
           <u>DISTILLERY</u>
-          <Text>
+          <Text alignSelf="flex-start">
             • Planned, scheduled, supervised, operated and maintained distilling
             equipment for company - SpecMech, Müeller.
           </Text>
-          <Text>
+          <Text alignSelf="flex-start">
             • Established workflow documentation that optimizes productivity and
             consistency.
           </Text>
 
-          <Text>
+          <Text alignSelf="flex-start">
             • Worked on a change management team, where a broad range of
             continuous improvement projects were completed, including a key
             software transition with no downtime.
           </Text>
 
-          <Text>•Key project reduced raw material usage by 20%.</Text>
+          <Text alignSelf="flex-start">
+            •Key project reduced raw material usage by 20%.
+          </Text>
         </>
-      ),
+      )
     },
     {
       role: "Head Distiller/Head Brewer",
@@ -75,54 +98,54 @@ export default function Work() {
       description: (
         <>
           <u>BREWERY</u>
-          <Text>
+          <Text alignSelf="flex-start">
             • Supervised staff, operated and maintained brewing equipment for
             company or contract use grain to glass - DME, SpecMech, Gia, KHS.
           </Text>
-          <Text>
+          <Text alignSelf="flex-start">
             • Hired employees, developed and trained teams, which extended to
             other departments.
           </Text>
-          <Text>
+          <Text alignSelf="flex-start">
             • Created/restructured recipes resulting in decreased product
             destruction, and enhanced ratings.
           </Text>
-          <Text>
+          <Text alignSelf="flex-start">
             • Planned/scheduled production, that increased ability to take on
             special projects. Effective stock and material requirement planning.
           </Text>
-          <Text>
+          <Text alignSelf="flex-start">
             • Created and organized production plans/records, SOP’s and quality
             control measures to ensure high quality product and compliance with
             BC-Canadian standards.
           </Text>
-          <Text>
+          <Text alignSelf="flex-start">
             • Enabled plant inspections for key outside parties and contract
             customers. Maintained fundamental benchmarks set by regulatory
             bodies and improved relations to gain new and less restrictive
             permits.
           </Text>
           <u>DISTILLERY</u>
-          <Text>
+          <Text alignSelf="flex-start">
             • Planned, scheduled, supervised, operated and maintained
             traditional Scotch-Irish style distilling equipment for company and
             contract use from grain to wood to glass - Forsyth, SpecMech.
           </Text>
-          <Text>
+          <Text alignSelf="flex-start">
             • Creation of inaugural production records, SOP’s and quality
             control measures to ensure final spirit quality and compliance with
             BC-Canadian standards.
           </Text>
-          <Text>
+          <Text alignSelf="flex-start">
             • Facilitated contractors on +$100,000 repair project of adiabatic
             cooler.
           </Text>
-          <Text>
+          <Text alignSelf="flex-start">
             • Part of special project teams - peat malt smoking, “Rick-House”,
             Irish style whiskey.
           </Text>
         </>
-      ),
+      )
     },
     {
       role: "Brewer",
@@ -133,69 +156,70 @@ export default function Work() {
       description: (
         <>
           <u>BREWERY</u>
-          <Text>
+          <Text alignSelf="flex-start">
             • Driftwood Brewery, Victoria BC. Worked and maintained brewing
             equipment, cellar, filtration and packaging - SpecMech, KHS, Alfa
             Laval.
           </Text>
-          <Text>• Organized production records and created SOP's.</Text>
+          <Text alignSelf="flex-start">
+            • Organized production records and created SOP's.
+          </Text>
         </>
-      ),
-    },
-    {
-      role: "Brewer - Packaging Operator",
-      company: "Brewdog, Ellon, Scotland",
-      timeline: "Apr 2016 - Apr 2017",
-      link: "https://www.brewdog.com/uk",
-      icon: "3466690.png",
-      description: (
-        <>
-          <u>BREWERY</u>
-          <Text>
-            • Performed as an acting supervisor of 3-4 team members, while
-            maintaining production records.
-          </Text>
-          <Text> • Trained staff on plant operations.</Text>
-          <Text>
-            • Operated and maintained brewhouse, cellar and filtration - Rolec,
-            AlfaLaval.
-          </Text>
-          <Text>
-            • Analyzed product in laboratory; leading to final decisions on
-            product.
-          </Text>
-          <Text>
-            • Contributed to plant safety and quality feature development and
-            internal projects.
-          </Text>
-          <u>PACKAGING</u>
-          <Text>
-            • Advised senior staff on initiatives, improvements and counselled
-            management regarding employee progression.
-          </Text>
-          <Text>
-            • Trusted to facilitate plant inspections - Tesco, Trading
-            Standards, Contract Customers.
-          </Text>
-          <Text>
-            • Surveyed 3-4 packaging assistants operated and maintained
-            equipment - KHS, Krones, EuroStar, while maintaining production
-            records.
-          </Text>
-          <Text> • Trained staff on procedure and machinery.</Text>
-        </>
-      ),
-    },
+      )
+    }
+    // {
+    //   role: "Brewer - Packaging Operator",
+    //   company: "Brewdog, Ellon, Scotland",
+    //   timeline: "Apr 2016 - Apr 2017",
+    //   link: "https://www.brewdog.com/uk",
+    //   icon: "3466690.png",
+    //   description: (
+    //     <>
+    //       <u>BREWERY</u>
+    //      <Text alignSelf="flex-start">
+    //         • Performed as an acting supervisor of 3-4 team members, while
+    //         maintaining production records.
+    //       </Text>
+    //      <Text alignSelf="flex-start"> • Trained staff on plant operations.</Text>
+    //      <Text alignSelf="flex-start">
+    //         • Operated and maintained brewhouse, cellar and filtration - Rolec,
+    //         AlfaLaval.
+    //       </Text>
+    //      <Text alignSelf="flex-start">
+    //         • Analyzed product in laboratory; leading to final decisions on
+    //         product.
+    //       </Text>
+    //      <Text alignSelf="flex-start">
+    //         • Contributed to plant safety and quality feature development and
+    //         internal projects.
+    //       </Text>
+    //       <u>PACKAGING</u>
+    //      <Text alignSelf="flex-start">
+    //         • Advised senior staff on initiatives, improvements and counselled
+    //         management regarding employee progression.
+    //       </Text>
+    //      <Text alignSelf="flex-start">
+    //         • Trusted to facilitate plant inspections - Tesco, Trading
+    //         Standards, Contract Customers.
+    //       </Text>
+    //      <Text alignSelf="flex-start">
+    //         • Surveyed 3-4 packaging assistants operated and maintained
+    //         equipment - KHS, Krones, EuroStar, while maintaining production
+    //         records.
+    //       </Text>
+    //      <Text alignSelf="flex-start"> • Trained staff on procedure and machinery.</Text>
+    //     </>
+    //   )
+    // }
   ];
 
   return (
-    <SimpleGrid
-      columns={1}
-      backgroundColor="brand.600"
-      // justifyContent="center"
-      w="70%"
-    >
-      <Box colSpan={1} style={{ boxShadow: "10px 10px #2D4739" }}>
+    <SimpleGrid columns={1} backgroundColor="brand.600" w={["92%", "70%"]}>
+      <Box
+        colSpan={1}
+        padding={[4, 8]}
+        style={{ boxShadow: "10px 10px #2D4739" }}
+      >
         <Center flexDirection="column">
           <Container
             flexDirection="column"
@@ -210,38 +234,40 @@ export default function Work() {
             <Text fontSize="sm">Previous roles.</Text>
           </Container>
 
-          <Stack className="jobs" w="100%" padding={4}>
-            <Accordion allowToggle>
+          <Stack className="jobs" w="100%">
+            <Accordion allowToggle mb={[5]}>
               {jobs.map((job) => {
                 return (
-                  <div key={job.company}>
-                    <AccordionItem border="transparent">
-                      <AccordionButton>
-                        <AccordionIcon /> {"   "}
-                        {job.role}
-                      </AccordionButton>
-                      <AccordionPanel margin={10}>
-                        <div align="center">
-                          <a href={job.link} target="blank">
-                            <img
-                              color="red"
-                              src={job.icon}
-                              alt="still"
-                              width="50"
-                              height="50"
-                            />
-                          </a>
-                          <p>
-                            {job.company.toUpperCase()} <br /> {job.timeline}{" "}
-                          </p>
-                        </div>
-
-                        <p className="job-description" align="left">
-                          {job.description}
-                        </p>
-                      </AccordionPanel>
-                    </AccordionItem>
-                  </div>
+                  <AccordionItem
+                    border="transparent"
+                    key={job.company}
+                    mt={[5]}
+                  >
+                    <AccordionButton>
+                      <AccordionIcon />
+                      {job.role}
+                    </AccordionButton>
+                    <AccordionPanel margin={[-1, 10]}>
+                      <Box display="flex" justifyContent={["center"]} m={[2]}>
+                        <a href={job.link} target="blank">
+                          <img
+                            color="red"
+                            src={job.icon}
+                            alt="still"
+                            width="50"
+                            height="50"
+                          />
+                        </a>
+                      </Box>
+                      <Box display="flex" flexDirection="column">
+                        <Text alignSelf="flex-start">
+                          {job.company.toUpperCase()}
+                        </Text>
+                        <Text alignSelf="flex-start">{job.timeline}</Text>
+                        {job.description}
+                      </Box>
+                    </AccordionPanel>
+                  </AccordionItem>
                 );
               })}
             </Accordion>
